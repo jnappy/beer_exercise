377f5f5c9450974497bbc9de0688f2c40eaeee28
Lindsey Truong <lindsey8417@gmail.com>
2015-06-28 12:09:50 -0400

    first version of beer

--- beer.rb
+++ beer.rb
@@ -1,4 +1,23 @@
 require 'pry'
+
+def can_drink_beer(age)
+	age = age.to_i
+
+	if age >= 22
+		puts "Yay, have a beer!"
+	elsif age == 21
+		puts "Happy Birthday, have a beer!"
+	else age <=20
+		puts "Nay, don't have a beer!"
+	end
+end
+
+
+puts "How old are you today?"
+
+user_age = gets.strip
+
+can_drink_beer(user_age)
  
 #write a function that accepts a variable: age
 #if age is greater than 21, display "Yay, have a beer!"
