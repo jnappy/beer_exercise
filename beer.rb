4fa761bb2bc493a991476c624cdbcfd94bf71fb7
marissapple <marissa@kickstarter.com>
2015-09-20 11:54:57 -0400

    Beer exercise from Marissa!

--- beer.rb
+++ beer.rb
@@ -1,11 +1,15 @@
 require 'pry'
- 
-#write a function that accepts a variable: age
-#if age is greater than 21, display "Yay, have a beer!"
-#if age is less than 21, display "Nay, don't have a beer!"
- 
-#ask for the user's age
- 
-#store the user input in a variable: user_age
- 
-#run the function
+
+def drinking_age(age)
+  if age.to_i > 21
+    puts "Yay, have a beer!"
+  else
+    puts "Nay, don't have a beer!"
+  end
+end
+
+puts "Hi there! How old are you?"
+
+user_age = gets
+
+drinking_age(user_age)
