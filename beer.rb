1695a899e0454d48f529dd9c10662330d8e5fff9
Tiffany Tzeng <tiffanytzeng@Tiffanys-MacBook-Air.local>
2015-09-27 13:58:41 -0400

    shenanigans

--- beer.rb
+++ beer.rb
@@ -1,11 +1,25 @@
 require 'pry'
- 
+
 #write a function that accepts a variable: age
+def beer(age)
+  if age >= "21"
+    puts("Yay, have a beer!")
+  else age < "21"
+    puts("Nay, don't have a beer!")
+  end
+end
+
+puts "How old are you?"
+user_age = gets.strip
+
+beer(user_age)
+
+
 #if age is greater than 21, display "Yay, have a beer!"
 #if age is less than 21, display "Nay, don't have a beer!"
- 
+
 #ask for the user's age
- 
+
 #store the user input in a variable: user_age
- 
+
 #run the function
