8dae715439712f4f11c53f63a8ba08e74abc172c
Dina Shin <dinashin@Dinas-MacBook-Air.local>
2015-09-27 14:19:13 -0400

    Final Version

--- beer.rb
+++ beer.rb
@@ -9,6 +9,12 @@ end
 
 end
 
+puts ("how old are you")
+
+user_age = gets.strip
+
+how_old(user_age)
+
 #write a function that accepts a variable: age
 #if age is greater than 21, display "Yay, have a beer!"
 #if age is less than 21, display "Nay, don't have a beer!"
