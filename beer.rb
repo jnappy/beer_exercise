74177ff00e7169b813629f9fa480c684e98b0845
marcela villa <a.marcela.v@gmail.com>
2015-09-27 14:04:21 -0400

    beer.rb

--- beer.rb
+++ beer.rb
@@ -1,5 +1,16 @@
 require 'pry'
- 
+ def happyhour(age)
+ 	if age >= 21
+ 		puts ("yay have a beer")
+ 	else
+ 		puts ("no beer for you") 
+ 	end 
+ end
+
+ 	puts "how old are you?"
+ 	user_age = gets.to_i
+
+ 	happyhour(user_age)
 #write a function that accepts a variable: age
 #if age is greater than 21, display "Yay, have a beer!"
 #if age is less than 21, display "Nay, don't have a beer!"
