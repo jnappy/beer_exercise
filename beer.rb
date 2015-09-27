8064d50345959e6b71d8eab35f02042e37892d20
Whitney Dobladilo <whitneydobladilo@Whitneys-MacBook-Pro.local>
2015-09-27 14:17:21 -0400

    beer.rb

--- beer.rb
+++ beer.rb
@@ -13,15 +13,15 @@ end
 
 	happy_hour(user_age)
 
- 	 
+
 #write a function that accepts a variable: age
 
 #if age is greater than 21, display "Yay, have a beer!"
 
 #if age is less than 21, display "Nay, don't have a beer!"
- 
+
 #ask for the user's age
- 
+
 #store the user input in a variable: user_age
- 
+
 #run the function
