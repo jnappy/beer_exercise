584aa2b7bb638891137d186d2164cffbdf07a839
Andrew Skewes <andrewskewes@Andrews-MacBook-Pro.local>
2015-09-27 14:04:32 -0400

    beer.rb

--- beer.rb
+++ beer.rb
@@ -1,11 +1,20 @@
 require 'pry'
  
 #write a function that accepts a variable: age
+def How_Old(age)
 #if age is greater than 21, display "Yay, have a beer!"
+	if age >= 21
+		puts("Yay, have a beer!")
 #if age is less than 21, display "Nay, don't have a beer!"
- 
+	else
+		puts("Nay, don't have a beer!")
+	end
+end
+
 #ask for the user's age
- 
+puts("How old are you?") 
 #store the user input in a variable: user_age
- 
+your_age = gets.to_i
 #run the function
+
+How_Old(your_age)
