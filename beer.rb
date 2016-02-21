39bec4f88a06a88dc4f804f6c6cb2e69c7a38c7d
eblack <ellenjeanblack@gmail.com>
2016-02-21 14:00:04 -0500

    Here is my beer exercise.

--- beer.rb
+++ beer.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
+def can_you_drink?(age)
+  puts(age)
+end
+
+puts("What is your age?")
+user_age = gets.strip
+if user_age >= ('21')
+  puts("Yay, have a beer!")
+elsif user_age < ('21')
+  puts("Nay, don't have a beer")
+
+end
+
+can_you_drink(user_age)
+
 #write a function that accepts a variable: age
 #if age is greater than 21, display "Yay, have a beer!"
 #if age is less than 21, display "Nay, don't have a beer!"
