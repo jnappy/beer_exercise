b2b1292d038bca7922a09eccb464ec6c4ecf9f98
sloiacon <samanthaloiacono5@gmail.com>
2016-02-21 14:10:31 -0500

    changes to beer

--- beer.rb
+++ beer.rb
@@ -1,4 +1,19 @@
-require 'pry'
+def how_old_are_you?
+puts("Hey, how old are you?")
+user_age = gets.strip
+if user_age >= "21"
+  puts("Yay, have a beer!")
+elsif user_age <= "20"
+  puts("Nay, don't have a beer!")
+else
+  puts("your input was ridiculous - run this file again")
+end
+end
+
+how_old_are_you?
+
+
+
 
 #write a function that accepts a variable: age
 #if age is greater than 21, display "Yay, have a beer!"
