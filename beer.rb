f1cabe42d4dedd520d6658e5949e6762950e2faa
rchada <rachel.chada@gmail.com>
2016-02-21 14:23:09 -0500

    beer changes

--- beer.rb
+++ beer.rb
@@ -1,11 +1,14 @@
 require 'pry'
 
-#write a function that accepts a variable: age
-#if age is greater than 21, display "Yay, have a beer!"
-#if age is less than 21, display "Nay, don't have a beer!"
+def what_is_your_age?(age)
+  if age >= 21
+    puts("Yay, have a beer")
+  else
+    puts("Nay, don't have a beer!")
+  end
+end
 
-#ask for the user's age
+puts("Hey, how old are you??")
+  user_age = gets.strip.to_i
 
-#store the user input in a variable: user_age
-
-#run the function
+what_is_your_age?(user_age)
