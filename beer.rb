fa196eabc57bd194ad6fb664366d6303bc5ee64a
Emily Stewart-Weeks <emilystewart-weeks@Emilys-MacBook-Air-2.local>
2016-03-13 15:31:06 -0400

    I DID IT

--- beer.rb
+++ beer.rb
@@ -1,6 +1,21 @@
 require 'pry'
 
 #write a function that accepts a variable: age
+
+def canwedrink(age)
+	if age > 21
+	puts "Yay, have a beer!"
+	elsif age < 21
+	puts "Nay, don't have a beer!"
+	end
+end
+
+puts "how old are you?"
+
+user_age = gets.to_i
+
+canwedrink(user_age)
+
 #if age is greater than 21, display "Yay, have a beer!"
 #if age is less than 21, display "Nay, don't have a beer!"
 
