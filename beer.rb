4fdf4a7e9bda4b261909706521042e57c784f427
Sam Tilin <stilin@NY1-C2QMR0EXFKYQ.local>
2016-03-13 15:29:17 -0400

    add birthday message

--- beer.rb
+++ beer.rb
@@ -3,9 +3,11 @@ require 'pry'
 #write a function that accepts a variable: age
 def beer(age)
 	#if age is greater than 21, display "Yay, have a beer!"
-	if age >= 21
+	if age > 21
 		puts "Yay, have a beer"
-	
+	#if ages is 21, wish the user a Happy Birthday
+	elsif age == 21
+		puts "Happy Birthday, buddy!"
 	#if age is less than 21, display "Nay, don't have a beer!"
 	else
 		puts "Nay, don't have a beer"
