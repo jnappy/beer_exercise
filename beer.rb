6757e37f3d95de2f0de0bcbc5ff1ccc56d73d897
Mehves Tangun <mehvestangun@Mehvess-MacBook-Air.local>
2016-03-13 15:20:14 -0400

    Mehves's beer file version

--- beer.rb
+++ beer.rb
@@ -1,11 +1,21 @@
 require 'pry'
 
 #write a function that accepts a variable: age
+def f(x)
+	if x > 21
+		puts "Yay, have a beer!"
+	else 
+		puts "Nay, don't have a beer!"
 #if age is greater than 21, display "Yay, have a beer!"
 #if age is less than 21, display "Nay, don't have a beer!"
+end
+end
+
+puts "how old are you?"
+
+user_age = gets.to_i
 
 #ask for the user's age
 
 #store the user input in a variable: user_age
-
-#run the function
+f(user_age)
