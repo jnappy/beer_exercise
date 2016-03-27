2a384eb372345faf2faec531fd2d72fd7a60b177
Paul Becker <paulbecker@Ericas-MacBook-Pro.local>
2016-03-27 13:20:56 -0400

    solved problem for beer

--- beer.rb
+++ beer.rb
@@ -4,8 +4,23 @@ require 'pry'
 #if age is greater than or equal to 21, display "Yay, have a beer!"
 #otherwise show them a message: "Nay, don't have a beer!"
 
+def function_name(x)
+
+  if x >= 21
+    puts("Yay, have a beer")
+  else
+    puts("Nay, don't have a beer")
+  end
+end
+
 #ask for the user's age
+
+puts("What is your age?")
 
 #store the user input in a variable: user_age
 
+user_age = gets.strip.to_i
+
 #run the function
+
+function_name(user_age)
