83cd53d7acc3b3960dfa7c736c4e04be9d186f80
Kathy <katherine.a.cuevas@gmail.com>
2016-06-19 15:54:51 -0400

    this is my work for the beer exercise

--- beer.rb
+++ beer.rb
@@ -5,7 +5,19 @@ require 'pry'
 #otherwise show them a message: "Nay, don't have a beer!"
 
 #ask for the user's age
+puts("what is your age?")
 
 #store the user input in a variable: user_age
 
+user_age = gets.strip.to_i
+
+def old_enough (number)
+	if number >= 21
+		puts("Yay, have a beer!")
+	else
+		puts("nay, don't have a beer")
+	end
+end
+
 #run the function
+old_enough(user_age)
