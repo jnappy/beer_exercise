252fce5c177309a15206d1fc82a64c32d5dc89d3
jnappy <jpnappy@gmail.com>
2016-10-23 12:53:55 -0400

    Update beer.rb

--- beer.rb
+++ beer.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 #write a function that accepts a variable: age
 #if age is greater than 21, display "Yay, have a beer!"
-#if age is equal to 21, display "Yay, have a beer, newbie!"
 #otherwise show them a message: "Nay, don't have a beer!"
 
 #ask for the user's age
