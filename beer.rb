b85e278f634b1c87d34c623ff74939d303223c23
The A-Team <alexisabrenica@Andrews-MacBook-Air.local>
2016-10-23 13:10:21 -0400

    Completed beer exercise

--- beer.rb
+++ beer.rb
@@ -4,8 +4,21 @@
 #if age is greater than 21, display "Yay, have a beer!"
 #otherwise show them a message: "Nay, don't have a beer!"
 
+def beer_age(age)
+	if age >= 21
+		puts("Yay, have a beer!")
+	else 
+		puts("Nay, don't have a beer!")
+	end
+end
+
 #ask for the user's age
+puts("I'm a lazy! Let me ask you what your age is instead of checking your id. What's your age?")
 
 #store the user input in a variable: user_age
+user_age = gets.strip
+user_age = user_age.to_i
 
 #run the function with the user_age variable
+
+beer_age(user_age)
